53063a0b5749e02961b3e94b120fcaeeb157f0d4
devmoura29 <dev.moura29@gmail.com>
2026-07-23 16:02:34 -0300

    Adiciona sistema de desconto por categoria de cliente

---
+++ calculadora_desconto.py
@@ -0,0 +1,16 @@
+categoria = input("Qual a categoria (vip ou comum)?")
+compra = float(input("Qual o valor da compra? "))
+if categoria == "vip":
+    if compra > 200:
+        valor_final = compra - (compra * 0.20)
+        print(f" Você teve 20% de desconto e vai pagar R$ {valor_final}")
+    else:
+        valor_final = compra - (compra * 0.10)
+        print(f"você teve 10% de desconto e vai pagar R$ {valor_final}")
+elif categoria == "comum":
+    if compra > 500:
+        valor_final = compra - (compra * 0.10)
+        print(f"você teve 10% de desconto e vai pagar R$ {valor_final}")
+    else:
+        valor_final = compra
+        print(f"sua compra saiu no valor R$ {valor_final}")
